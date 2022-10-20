cb35a049305ec917b60aaf82ae440663b00a23b7
linox2000 <mllino1@hotmail.com>
2022-10-20 17:04:33 +0100

    finished regex validation using Imask

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./css/index.css";
-import IMask from 'imask';
-
+import IMask from "imask";
 
 const ccBgColor01 = document.querySelector(
   ".cc-bg svg > g g:nth-child(1) path"
@@ -9,22 +8,70 @@ const ccBgColor01 = document.querySelector(
 const ccBgColor02 = document.querySelector(
   ".cc-bg svg > g g:nth-child(2) path"
 );
-const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
-const securityCode = document.querySelector('#security-code')
+const securityCode = document.querySelector("#security-code");
 const securityCodePatten = {
-  mask:'0000'
-}
-  const securityCodeMaked = IMask(securityCode,securityCodePatten)
+  mask: "0000",
+};
+const securityCodeMaked = IMask(securityCode, securityCodePatten);
 
-function setCardType(type){
-    const colors = {
-      visa: ["#436D99", "#2D57F2"],
-      mastercard: ["#DF6F29", "#C69347"],
-      default: ["black", "gray"],
-    };
-    ccBgColor01.setAttribute('fill',colors[type][0])
-    ccBgColor02.setAttribute('fill',colors[type][1])
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
 
-    ccLogo.setAttribute('src',`cc-${type}.svg`)
+const cardNumber = document.querySelector("#card-number");
+
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    var number = (dynamicMasked.value + appended).replace(/\D/g, "");
+    const foundMask = dynamicMasked.compiledMasks.find((item) =>
+      number.match(item.regex)
+    );
+    return foundMask
+  },
+};
+
+const cardNumbercardMasked = IMask(cardNumber,cardNumberPattern)
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  };
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
